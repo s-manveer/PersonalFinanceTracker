768359eb63ba68a254f6c9d1c52cffab54d4a153
Manveer Singh <manveer.singh11@gmail.com>
2025-06-17 02:34:37 +0530

    Initial commit

---
+++ PersonalFinanceTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PersonalFinanceTracker
+//
+//  Created by Manveer Singh on 17/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PersonalFinanceTracker/PersonalFinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalFinanceTrackerApp.swift
+//  PersonalFinanceTracker
+//
+//  Created by Manveer Singh on 17/06/25.
+//
+
+import SwiftUI
+
+@main
+struct PersonalFinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
